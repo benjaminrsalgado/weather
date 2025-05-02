c03d0151191d5e2de734cff602165f55e1637dae
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-02 10:55:42 -0600

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Benjamin Rojo on 02/05/25.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
